4c9c55990ea1f7bbf30d00c08e0cbedeacdecf3b
Alfat1989 <gilmalf89@gmail.com>
2022-05-27 20:36:54 +0300

    add App file

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// import { render } from 'react-dom'
+import Painting from 'components/Painting'
+
+export default function App() {
+    return (
+        <div>
+            <Painting />
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+import { render } from 'react-dom';
+// import React from 'react';
+// import peintings from './paintings.json'
+import App from './App'
+  
+
+// function Peinting(props) {
+//   return (
+//     <App />, document.querySelector('#root')
+//   )
+// }
+
+
+render(<App />,
+  document.querySelector('#root'))
+
+
+// ===============================
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import { App } from 'components/App';
@@ -8,6 +26,6 @@
 //     <App />
 //   </React.StrictMode>
 // );
-
+// ==================================
 
 console.log('hi')
